7dec3820102831a37f9f320ad8f97209d429ad92
badr.naji <eddine_naji.badr@bitlc.onmicrosoft>
2023-08-02 12:17:37 +0200

    1

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Component({
+  selector: 'bm-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent {
+input$=new Subject<string>();3
+}
